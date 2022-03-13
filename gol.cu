6a02536e92931210caba2fb11c86a43485f56381
sorrownight <34826771+sorrownight@users.noreply.github.com>
2022-03-13 13:40:47 -0700

    - Implemented graphics & fixed bugs

--- gol.cu
+++ gol.cu
@@ -1,3 +1,6 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
 #include <chrono>
 #include <ctime>
 #include <functional>
@@ -5,29 +8,23 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <GL/glut.h>
+#include <conio.h>
+#include <thread>
 
-const unsigned int THREAD_1D = 21;
-const unsigned int BLOCK_AREA = THREAD_1D * THREAD_1D;
-const unsigned int BLOCK_1D = 2;
-const unsigned int DIM = (BLOCK_AREA * BLOCK_1D);
+const unsigned int THREAD_1D = 16;
+//const unsigned int BLOCK_AREA = THREAD_1D * THREAD_1D;
+const unsigned int BLOCK_1D = 800;
+const unsigned int DIM = (THREAD_1D * BLOCK_1D);
 // Reserving 4 rows/cols of 0s on the border to eliminate edge cases
 
-const unsigned int GEN_COUNT = 5000;
+const unsigned int GEN_COUNT = 200;
 const std::string DEFAULT_PATTERN_FILE = "data.txt";
 
-__device__ static unsigned int getRow()
-{
-    return threadIdx.y + blockDim.y * DIM;
-}
-
-__device__ static unsigned int getCol()
-{
-    return threadIdx.x + blockDim.x * DIM;
-}
 
 // Each generation takes up a space of DIM * DIM
 // Each row takes up a space of DIM -> offset by col
-__device__ static unsigned int getActualIdx(unsigned int gen, unsigned int row, unsigned int col)
+__forceinline __device__ static unsigned int getActualIdx(unsigned int gen, unsigned int row, unsigned int col)
 {
     unsigned int genStart = gen * DIM * DIM; // Each subarray has DIM * DIM length
     return genStart + row * DIM + col;
@@ -35,8 +32,8 @@ __device__ static unsigned int getActualIdx(unsigned int gen, unsigned int row, 
 
 __global__ void nextGen(bool* gens, int curGen)
 {
-    const unsigned int row = getRow();
-    const unsigned int col = getCol();
+    const unsigned int row = threadIdx.y + blockIdx.y * blockDim.y;
+    const unsigned int col = threadIdx.x + blockIdx.x * blockDim.x;
 
     // We want some threads working on the reserved rows/cols of zero's too! (For performance)
     if (row == 0 || col == 0 || row == DIM - 1 || col == DIM - 1)
@@ -59,20 +56,27 @@ __global__ void nextGen(bool* gens, int curGen)
 
 bool* firstIterLife(const bool* initialGrid)
 {
-    dim3 dimGrid(BLOCK_1D, BLOCK_1D, 1);
-    dim3 dimBlock(THREAD_1D, THREAD_1D, 1);
+    dim3 dimGrid(BLOCK_1D, BLOCK_1D);
+    dim3 dimBlock(THREAD_1D, THREAD_1D);
 
     bool* d_gens;
-    cudaMalloc((void**) &d_gens, GEN_COUNT * DIM * DIM);
+    if (cudaMalloc((void**) &d_gens, (GEN_COUNT * DIM * DIM) * sizeof(bool)) != cudaSuccess)
+        std::cout << "cudaMalloc failed" << std::endl;
 
     // First generation needs to be copied over to start the algorithm
-    cudaMemcpy(d_gens, initialGrid, DIM * DIM, cudaMemcpyHostToDevice);
+    if (cudaMemcpy(d_gens, initialGrid, DIM * DIM * sizeof(bool), cudaMemcpyHostToDevice) != cudaSuccess)
+        std::cout << "cudaMemcpy failed: Host->Device" << std::endl;
 
     for (int gen = 1; gen < GEN_COUNT; gen++) {
+        //cudaDeviceSynchronize();
         nextGen<<<dimGrid, dimBlock>>>(d_gens, gen);
     }
     bool *result = new bool[GEN_COUNT*DIM*DIM];
-    cudaMemcpy(result, d_gens, GEN_COUNT * DIM * DIM, cudaMemcpyDeviceToHost);
+    cudaError_t code;
+    if ((code = cudaMemcpyAsync(result, d_gens, (GEN_COUNT * DIM * DIM) * sizeof(bool), cudaMemcpyDeviceToHost)) != cudaSuccess)
+        std::cout << "cudaMemcpy failed: Device->Host: " << cudaGetErrorString(code) << std::endl;
+    
+    cudaFree(d_gens);
 
     return result;
 }
@@ -122,21 +126,6 @@ bool* serialLife(const bool* initialGrid)
     return gens;
 }
 
-bool* measureTime(const std::function<bool*(const bool*)>& func,
-                 const bool* initialGrid)
-{
-    using namespace std::chrono;
-
-    auto start = system_clock::now();
-    bool* result = func(initialGrid);
-    auto end = system_clock::now();
-
-    std::cout << "Execution Time: " << (duration_cast<milliseconds>(end-start)).count()
-                << " milliseconds" << std::endl;
-
-    return result;
-}
-
 bool* createGridFromFile()
 {
     std::vector<std::vector<bool>> dataGrid;
@@ -169,7 +158,63 @@ bool* createGridFromFile()
     return initialGrid;
 }
 
-int main()
+float getCoord(int gridIdx)
+{
+    return ((float)gridIdx * 2) / DIM - 1;
+}
+
+bool* g_lifeGrid; // because OpenGL is dumb
+
+void display() 
+{ 
+    
+    glColor3f(0.0f, 0.0f, 0.0f);
+    for (int gen = 0; gen < GEN_COUNT; gen++) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        for (int row = 0; row < DIM; row++) {
+            for (int col = 0; col < DIM; col++) {
+                if (g_lifeGrid[getActualIdxHost(gen, row, col)])
+                    glRectf(getCoord(col), getCoord(row), getCoord(col + 1), getCoord(row + 1));             
+            }
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        glutSwapBuffers();
+    } 
+    
+}
+
+void graphics(bool* lifeGrid)
+{
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+    glutInitWindowSize(DIM + 20, DIM + 20);
+    glutInitWindowPosition(10, 10);
+    glutCreateWindow("CUDA Game of Life");
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    g_lifeGrid = lifeGrid;
+
+    glutDisplayFunc(display);
+
+    glutMainLoop();
+
+    getch();//pause here to see results or lack there of
+}
+
+bool* measureTime(const std::function<bool* (const bool*)>& func,
+    const bool* initialGrid)
+{
+    using namespace std::chrono;
+
+    auto start = system_clock::now();
+    bool* result = func(initialGrid);
+    auto end = system_clock::now();
+
+    std::cout << "Execution Time: " << (duration_cast<milliseconds>(end - start)).count()
+        << " milliseconds" << std::endl;
+
+    return result;
+}
+
+int main(int argc, char* argv[])
 {
 
     bool* initialGrid = createGridFromFile();
@@ -180,12 +225,11 @@ int main()
 
     std::cout << "Running " << GEN_COUNT << " generations on grid of " << DIM << "x" << DIM << std:: endl;
 
-    std::cout << "Serial version: " << std:: endl;
-    bool* serialResult = measureTime(serialLife, initialGrid);
-
-
-    std::cout << "CUDA version: " << std:: endl;
+    std::cout << "CUDA version: " << std::endl;
     bool* cudaResult = measureTime(firstIterLife, initialGrid);
+
+    std::cout << "Serial version: " << std:: endl;
+    bool* serialResult = measureTime(serialLife, initialGrid);  
 
     bool matched = true;
     for (int row = 0; row < DIM && matched; row++) {
@@ -193,12 +237,15 @@ int main()
             if (  serialResult[getActualIdxHost(GEN_COUNT-1,row,col)]
                != cudaResult[getActualIdxHost(GEN_COUNT-1,row,col)]) {
                 matched = false;
+                std::cout << "Failed at row " << row << " col " << col << std:: endl;
             }
         }
     }
 
     if (!matched) std::cout << "Not matched!" << std:: endl;
     else std::cout << "Last generation of serial vs CUDA matched!" << std:: endl;
+
+    graphics(cudaResult);
 
     delete[] initialGrid;
     delete[] serialResult;
